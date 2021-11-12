d5a30e77d9212c87308a59e02dc57a81c3fcc0ee
Eddie <70488539+edbart@users.noreply.github.com>
2021-11-11 20:26:30 -0600

    got phaser working, currently using example.

---
+++ prebundle/canvas.js
@@ -0,0 +1,58 @@
+'use strict';
+document.addEventListener('DOMContentLoaded', function () {
+    // event handlers
+    var optbtn = document.getElementById('optionsBtn'), dimbg = document.getElementById('dimBg'), optdiv = document.getElementById('optionsDiv'), optclose = document.getElementById('closeOptions');
+    // options button
+    optbtn.addEventListener('click', function () {
+        dimbg.hidden = false;
+        optdiv.hidden = false;
+    });
+    // dim background, closes options menu
+    dimbg.addEventListener('click', function () {
+        dimbg.hidden = true;
+        optdiv.hidden = true;
+    });
+    // close options
+    optclose.addEventListener('click', function () {
+        dimbg.hidden = true;
+        optdiv.hidden = true;
+    });
+    var Phaser = require('phaser');
+    var config = {
+        type: Phaser.CANVAS,
+        canvas: document.getElementById('canvas'),
+        width: 500,
+        height: 500,
+        physics: {
+            "default": 'arcade',
+            arcade: {
+                gravity: { y: 200 }
+            }
+        },
+        scene: {
+            preload: preload,
+            create: create
+        }
+    };
+    var game = new Phaser.Game(config);
+    function preload() {
+        this.load.setBaseURL('http://labs.phaser.io');
+        this.load.image('sky', 'assets/skies/space3.png');
+        this.load.image('logo', 'assets/sprites/phaser3-logo.png');
+        this.load.image('red', 'assets/particles/red.png');
+    }
+    function create() {
+        this.add.image(400, 300, 'sky');
+        var particles = this.add.particles('red');
+        var emitter = particles.createEmitter({
+            speed: 100,
+            scale: { start: 1, end: 0 },
+            blendMode: 'ADD'
+        });
+        var logo = this.physics.add.image(400, 100, 'logo');
+        logo.setVelocity(100, 200);
+        logo.setBounce(1, 1);
+        logo.setCollideWorldBounds(true);
+        emitter.startFollow(logo);
+    }
+}); /*ends domcontentloaded*/
---
+++ src/canvas.ts
@@ -0,0 +1,79 @@
+'use strict';
+document.addEventListener('DOMContentLoaded', () => {
+
+// event handlers
+const optbtn = document.getElementById('optionsBtn'),
+dimbg = document.getElementById('dimBg'),
+optdiv = document.getElementById('optionsDiv'),
+optclose = document.getElementById('closeOptions');
+
+// options button
+optbtn.addEventListener('click', () => {
+    dimbg.hidden = false;
+    optdiv.hidden = false;
+});
+
+// dim background, closes options menu
+dimbg.addEventListener('click', () => {
+    dimbg.hidden = true;
+    optdiv.hidden = true;
+});
+
+// close options
+optclose.addEventListener('click', () => {
+    dimbg.hidden = true;
+    optdiv.hidden = true;
+});
+
+const Phaser = require('phaser');
+
+var config = {
+    type: Phaser.CANVAS,
+    canvas: document.getElementById('canvas'),
+    width: 500,
+    height: 500,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 200 }
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+    this.load.setBaseURL('http://labs.phaser.io');
+
+    this.load.image('sky', 'assets/skies/space3.png');
+    this.load.image('logo', 'assets/sprites/phaser3-logo.png');
+    this.load.image('red', 'assets/particles/red.png');
+}
+
+function create ()
+{
+    this.add.image(400, 300, 'sky');
+
+    var particles = this.add.particles('red');
+
+    var emitter = particles.createEmitter({
+        speed: 100,
+        scale: { start: 1, end: 0 },
+        blendMode: 'ADD'
+    });
+
+    var logo = this.physics.add.image(400, 100, 'logo');
+
+    logo.setVelocity(100, 200);
+    logo.setBounce(1, 1);
+    logo.setCollideWorldBounds(true);
+
+    emitter.startFollow(logo);
+}
+
+}); /*ends domcontentloaded*/
